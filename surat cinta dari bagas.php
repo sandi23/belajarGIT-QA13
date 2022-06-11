92d14711b187da43e393122dbd8b262f2469e235
Bagas Gizkara <gizkarabagas98@gmail.com>
2022-06-11 11:04:22 +0700

    kedua

---
+++ surat cinta dari bagas.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Belajar Git QA Cilsy</title>
+</head>
+<body>
+<p>Hello Semua, kita sedang belajar Git</p>
+</body>
+<p>Hello Semua, kita sedang belajar Git</p>
+</body>
+<p>Hello Semua, gggg</p>
+</html>
